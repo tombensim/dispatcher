7ba8a95d3d59d765fe5605f6b575e3ec281f06be
tom <tom@genoox.com>
2020-02-08 20:19:59 +0200

    react-create-app on Heroku

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+// @ts-ignore
+import { ReactMic }from 'react-mic';
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, updateState] = useState({record: true});
+    const startRecording = () => {
+        updateState({
+            ...state,
+            record: true
+        });
+    }
+
+    const stopRecording = () => {
+        updateState({
+            ...state,
+            record: false
+        });
+    };
+
+    const onData = (recordedBlob: Blob): void => {
+        console.log('chunk of real-time data is: ', recordedBlob);
+    }
+
+    const onStop = (recordedBlob: Blob): void => {
+        console.log('recordedBlob is: ', recordedBlob);
+    }
+    return (
+        <>
+            <div className="App">
+                <ReactMic
+                    record={state.record}
+                    className="sound-wave"
+                    onStop={onStop}
+                    onData={onData}
+                    strokeColor="#000000"
+                    backgroundColor="#FF4081"/>
+                <button onClick={startRecording} type="button">Start</button>
+                <button onClick={stopRecording} type="button">Stop</button>
+            </div>
+        </>
+    );
 }
 
 export default App;
